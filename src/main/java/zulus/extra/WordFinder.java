e4d1e6f92b9e352acd4848b72733d45c26a7cad8
Zulus <danil.kazim.99@gmail.com>
2018-04-12 12:31:35 +0300

    native search

--- src/main/java/zulus/extra/WordFinder.java
+++ src/main/java/zulus/extra/WordFinder.java
@@ -1,8 +1,14 @@
 package zulus.extra;
 
-import java.util.Map;
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class WordFinder {
+    private static final int PRIME_NUMBER = 31;
+    private static final int NORMILIZER= Integer.MAX_VALUE;
     /**
      * Given a String (the haystack) and an array of Strings (the needles),
      * return a Map<String, Integer>, where keys in the map correspond to
@@ -11,16 +17,44 @@ public class WordFinder {
      * was found. A needle that was not found in the haystack should not be
      * returned in the output map.
      *
-     * @param haystack
-     *            The string to search into.
-     * @param needles
-     *            The array of strings to search for. This array is not mutated.
+     * @param haystack The string to search into.
+     * @param needles  The array of strings to search for. This array is not mutated.
      * @return The list of needles that were found in the haystack.
      */
     public static Map<String, Integer> findWords(String haystack,
                                                  String[] needles) {
 
-        return null;
+        if (haystack == null || needles == null) return new HashMap<>();;
+        return rabinKarp(haystack, needles);
+//        return nativeSearch(haystack,needles);
     }
 
+    private static Map<String,Integer> nativeSearch(String haystack, String[] needles){
+        HashMap<String, Integer> output = new HashMap<>();
+        for(String needle:needles){
+            int index=haystack.indexOf(needle);
+            if(index>=0) {
+                output.put(needle, index);
+            }
+        }
+return output;
+    }
+
+    private static Map<String, Integer> rabinKarp(String haystack, String[] needles) {
+        HashMap<String, Integer> output = new HashMap<>();
+        List<Long> hashesNeedles = Arrays.stream(needles).map((needle) -> hash(needle)).collect(Collectors.toList());
+
+        return output;
+    }
+
+    private static long hash(String str) {
+        long powOfPN = 1;
+        long hash = 0;
+        for (char c : str.toCharArray()) {
+            powOfPN *= PRIME_NUMBER;
+            hash += powOfPN * c;
+            hash%=NORMILIZER;
+        }
+        return hash;
+    }
 }
