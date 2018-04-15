0bad28d4e0062986f7b1584cc222f314140e18a9
Zulus <danil.kazim.99@gmail.com>
2018-04-15 11:22:00 +0300

    fix extra string in pi search

--- src/main/java/zulus/extra/StringInPiFinder.java
+++ src/main/java/zulus/extra/StringInPiFinder.java
@@ -3,18 +3,18 @@ package zulus.extra;
 import java.util.Map;
 
 public class StringInPiFinder extends WordFinder {
-    public static int search(String str, int maxLength) {
-        if (str == null) return -1;
-        int length = 1000;
+    public static int search(String str) {
+        if (str == null|| !str.matches("\\d+")) return -1;
+        long length = 1000;
         char[] digits = "0123456789".toCharArray();
         do {
-            String pi = DigitsToStringConverter.convert(BaseTranslator.convertBase(PiGenerator.computePiInHex(length), 16, 10, length), 10, digits);
+            String pi = DigitsToStringConverter.convert(BaseTranslator.convertBase(PiGenerator.computePiInHex((int)length), 16, 10, (int)length), 10, digits);
             int matches = WordFinder.findWord(pi, str);
             if (matches >= 0) {
                 return matches;
             }
             length *= 5;
-        } while (length <= Integer.MAX_VALUE && (maxLength <= 0 || length < maxLength));
+        } while (length <= Integer.MAX_VALUE);
         return -1;
     }
 }
--- src/test/java/zulus/extra/StringInPiFinderTest.java
+++ src/test/java/zulus/extra/StringInPiFinderTest.java
@@ -9,24 +9,24 @@ import org.junit.jupiter.api.Disabled;
 public class StringInPiFinderTest {
     @Test
     public void emptyStringTest() {
-        assertEquals(-1, StringInPiFinder.search(null,0));
+        assertEquals(-1, StringInPiFinder.search(null));
     }
 
     @Test
     public void matchAtStartOfPiTest() {
-        assertEquals(0, StringInPiFinder.search("1",0));
-        assertEquals(0, StringInPiFinder.search("14",0));
-        assertEquals(0, StringInPiFinder.search("141",0));
+        assertEquals(0, StringInPiFinder.search("1"));
+        assertEquals(0, StringInPiFinder.search("14"));
+        assertEquals(0, StringInPiFinder.search("141"));
     }
 
 
     @Test
     @Disabled
     public void matchAtMiddleOfPiTest() {
-        assertEquals(2398, StringInPiFinder.search("0601",0));
+        assertEquals(2398, StringInPiFinder.search("0601"));
     }
     @Test
     public void doNotMatchLetterTest() {
-        assertEquals(-1, StringInPiFinder.search("A",100));
+        assertEquals(-1, StringInPiFinder.search("A"));
     }
 }
